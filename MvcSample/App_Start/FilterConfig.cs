bfc39dc8f6221561447083d1a2b7c0dbd075fab7
Krishna <krishna.medida@gmail.com>
2016-01-26 12:38:23 +0530

    Update FilterConfig.cs

--- MvcSample/App_Start/FilterConfig.cs
+++ MvcSample/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace MvcSample
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //Checking
         }
     }
 }
